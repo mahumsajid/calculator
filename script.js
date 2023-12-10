2906fa4c7cba2ce987280cb0855a6245109201df
Mahum <mahumxsajid@gmail.com>
2023-12-09 20:04:17 -0500

    Add event handlers to digits in order to have them display on calculator

--- script.js
+++ script.js
@@ -34,3 +34,24 @@ function operate(num1, num2, operator) {
     }
 
 }
+
+function getAnswer() {
+
+    let answer = document.querySelector(".answer");
+    let display = document.querySelector(".display");
+    let digits = document.querySelectorAll(".digit");
+    //let displayAnswer = answer.textContent;
+
+    digits.forEach(digit => {
+        digit.addEventListener("click", event => {
+            if (answer.textContent === "0" && digit.textContent !== ".") {
+                answer.textContent = digit.textContent;
+            } else {
+                answer.textContent = answer.textContent + digit.textContent;
+            }
+        });
+    });
+
+}
+
+getAnswer();
