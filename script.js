7a46c40eaf5658fff3bc91e2cb3c890e335ce9dc
Mahum <mahumxsajid@gmail.com>
2023-12-10 17:24:32 -0500

    Add error handling for divisions where decimals are too long for display by rounding decimals that are greater than 9 places

--- script.js
+++ script.js
@@ -10,9 +10,22 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-function divide(num1, num2) {
-    return num1 / num2;
+function countDecimals(number) {
+    return number.toString().split(".")[1].length;
 }
+
+function divide(num1, num2) {
+    if (num1 % num2 === 0) {
+        return num1 / num2;
+    } else {
+        let decimals = countDecimals(num1 / num2);
+        if (decimals > 9) {
+            return (num1 / num2).toFixed(9);
+        } 
+        return num1 / num2;
+    }
+}
+
 
 let num1, num2, op;
 
